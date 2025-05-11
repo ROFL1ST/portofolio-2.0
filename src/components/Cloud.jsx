02dcc14f61ebf4f61d141c5c5f038bd1347efd03
ROFL1ST <danendrapr55@gmail.com>
2025-05-11 16:22:56 +0700

    fix: swiper pagination

--- src/components/Cloud.jsx
+++ src/components/Cloud.jsx
@@ -8,7 +8,7 @@ export default function Cloud() {
     height="80"
     viewBox="0 0 64 64"
     fill="none"
-    className="cloud animate-cloud"
+    className="cloud  animate-cloud"
   >
     <path
       d="M48 24c-1.61 0-3.16.22-4.63.62C41.17 22.4 39.17 20 36 20c-3.26 0-6.16 1.63-7.88 4.05C25.61 23.01 23 22 20 22c-7.73 0-14 6.27-14 14s6.27 14 14 14h24c7.73 0 14-6.27 14-14s-6.27-14-14-14z"
